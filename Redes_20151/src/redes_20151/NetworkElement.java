177fbf6bc70d51d220163cfad23f047075dcde24
lfbonder <lorenzo.bonder@acad.pucrs.br>
2015-06-08 18:46:43 -0300

    Update NetworkElement.java

--- Redes_20151/src/redes_20151/NetworkElement.java
+++ Redes_20151/src/redes_20151/NetworkElement.java
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+ 
+ //Important for the work structure - Does the IP conversion and the base interface for Router and Network
 package redes_20151;
 
 /**
